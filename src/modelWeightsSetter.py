18d5c7f6dd08116ed41cc2c86a75bd30ce1f2074
Vitor Amaral <vitorsamaral@gmail.com>
2014-10-02 18:34:51 -0300

    Added the print of the output file.

--- src/modelWeightsSetter.py
+++ src/modelWeightsSetter.py
@@ -4,6 +4,13 @@ Created on 19/09/2014
 @author: vitor
 '''
 
+import cPickle as pickle
+import hypergraph
+import pprint
+
 if __name__ == "__main__" :
     with open('saida.pickle', 'r') as f:
+        output = pickle.load(f)
+        f = open('picPrint.txt', 'w')
+        pprint.pprint(output, f, 1)
         pass
