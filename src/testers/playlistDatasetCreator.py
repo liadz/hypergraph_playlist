bbd98c8f7c9500d60c4ebb19d41f6c85894a0787
Vitor Amaral <vitorsamaral@gmail.com>
2015-04-28 19:26:14 -0300

    time to have my own playlists for training

---
+++ src/testers/playlistDatasetCreator.py
@@ -0,0 +1,3 @@
+__author__ = 'vitor'
+#this modelu is going to get songs from the bigger playlist dataset to create a smaller more controlled one.
+#if possible Ill use made up songs which Id have more control.
